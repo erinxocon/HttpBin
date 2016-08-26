18a800d3ce5eb2a9b51a218d8443e9eab4012407
Erin O'Connell <erinocon5@gmail.com>
2016-08-26 17:28:27 -0600

    Added Unpack.cs to provide overload method for converting to dictionaries

---
+++ src/HttpBin/Utilis/Unpack.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace HttpBin.Utilis
+{
+    static public class Unpack
+    {
+        static public Dictionary<string, string> convertToDict(IQueryCollection queryString)
+        {
+            var newDict = new Dictionary<string, string>();
+            foreach (var entry in queryString)
+            {
+                newDict.Add(entry.Key, entry.Value);
+            }
+            return newDict;
+        }
+
+        static public Dictionary<string, string> convertToDict(IHeaderDictionary headerDict)
+        {
+            var newDict = new Dictionary<string, string>();
+            foreach (var entry in headerDict)
+            {
+                newDict.Add(entry.Key, entry.Value);
+            }
+            return newDict;
+        }
+    }
+}
