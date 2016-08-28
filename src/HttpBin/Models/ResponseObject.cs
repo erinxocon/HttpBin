b5968def636da06b638d96a6eb2d19a689768b63
Erin O'Connell <erinocon5@gmail.com>
2016-08-27 19:57:03 -0600

    Added a POCO ResponseObject

---
+++ src/HttpBin/Models/ResponseObject.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpBin.Models
+{
+    public class ResponseObject
+    {
+        public Dictionary<string, string> args { get; set; }
+        public Dictionary<string, string> headers { get; set; }
+        public string origin { get; set; }
+        public string url { get; set; }
+        public JObject json { get; set; }
+    }
+}
