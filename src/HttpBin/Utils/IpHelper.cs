31e10a517a5b47cb5ca060722669469d4610b887
Erin O'Connell <erinocon5@gmail.com>
2016-08-31 19:56:15 -0600

    Added class and method for detecting the proper ip if it's behind a reverse proxy

---
+++ src/HttpBin/Utils/IpHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace HttpBin.Utils
+{
+    public class IpHelper
+    {
+
+        static public string getIp(HttpRequest request)
+        {
+            string ipAddress;
+
+            if (request.Headers.Keys.Contains("X-Forwarded-For"))
+            {
+                ipAddress = request.Headers["X-Forwarded-For"];
+            }
+
+            else
+            {
+                ipAddress = request.HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+
+            return ipAddress;
+        }
+    }
+}
